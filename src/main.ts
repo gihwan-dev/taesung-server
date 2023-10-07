90371dbb4da8790769f72ae25ca7224cfda88122
최기환 <rlghks3004@gmail.com>
2023-10-07 14:15:32 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+// import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "https://taesung-26b6fexaf-gihwan-dev-s-team.vercel.app",
     credentials: true,
   });
-  const config = new DocumentBuilder()
-    .setTitle("Taesung")
-    .setDescription("Taesung API")
-    .setVersion("1.0")
-    .addTag("Taesung")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
-  await app.listen(process.env.PORT || 8000);
+  // const config = new DocumentBuilder()
+  //   .setTitle("Taesung")
+  //   .setDescription("Taesung API")
+  //   .setVersion("1.0")
+  //   .addTag("Taesung")
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup("api", app, document);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
