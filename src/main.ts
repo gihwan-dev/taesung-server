bd6240965463f737e97a3cbe758ec9f590bd26fe
최기환 <rlghks3004@gmail.com>
2024-01-12 16:16:45 +0900

    포트 추가 및 기본 포트 8080으로 초기화

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   //   .build();
   // const document = SwaggerModule.createDocument(app, config);
   // SwaggerModule.setup("api", app, document);
-  await app.listen(process.env.PORT || 8000);
+  await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();
